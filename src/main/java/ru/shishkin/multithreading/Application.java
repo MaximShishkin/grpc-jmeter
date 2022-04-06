9956141da7e6055270246f7cec2ecd2dbe90a505
MaximShishkin <yacyfra@gmail.com>
2022-04-06 19:41:00 +0300

    update files

--- src/main/java/ru/shishkin/multithreading/Application.java
+++ src/main/java/ru/shishkin/multithreading/Application.java
@@ -1,28 +1,24 @@
 package ru.shishkin.multithreading;
 
 public class Application {
+    public static void main(String[] args) {
+        System.out.println("Запускаем музыку во втором потоке");
+        NewThread t = new NewThread("MyNewTread");
+        System.out.println("Продолжаем выполнять какие-то действия в первом потоке");
+        while (true) {
+            runSto();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Запускаем музыку во втором потоке");
-		NewThread t = new NewThread("MyNewTread");
-		System.out.println("Продолжаем выполнять какие-то действия в первом потоке");
-		while(true)
-		{
-			sto();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	private static void sto()
-	{
-		for(int i = 0; i < 100; i++)
-		{
-			System.out.println("Шаг " + i);
-			System.out.println("Продолжаем выполнять какие-то действия в первом потоке");
-		}
-	}
+    private static void runSto() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Шаг " + i);
+            System.out.println("Продолжаем выполнять какие-то действия в первом потоке");
+        }
+    }
 }
--- src/main/java/ru/shishkin/multithreading/NewThread.java
+++ src/main/java/ru/shishkin/multithreading/NewThread.java
@@ -6,33 +6,30 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class NewThread extends Thread{
-	
-	public NewThread(String name)
-	{
-		setName(name);
-		start();
-	}
-	
-	public void run() {
-		while(go());
-	}
+public class NewThread extends Thread {
+    public NewThread(String name) {
+        setName(name);
+        start();
+    }
 
-	private boolean go()
-	{
-		try {
-			InputStream music = getClass().getClassLoader().getResourceAsStream("music.mp3");
-			Player player = new Player(music);
-			player.play();
-			player.close();
-			music.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (JavaLayerException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return true;
-	}
+    public void run() {
+        while (go()) ;
+    }
+
+    private boolean go() {
+        try {
+            InputStream music = getClass().getClassLoader().getResourceAsStream("music.mp3");
+            Player player = new Player(music);
+            player.play();
+            player.close();
+            music.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (JavaLayerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
 }
